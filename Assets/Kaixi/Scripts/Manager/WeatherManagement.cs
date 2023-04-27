af0cfa1a6bc107468d6979ffe446ece8cecf1058
Box <heziyin@hotmail.com>
2023-04-28 01:33:47 +0800

    add few functions to weathermanagement and create fog shader

--- Assets/Kaixi/Scripts/Manager/WeatherManagement.cs
+++ Assets/Kaixi/Scripts/Manager/WeatherManagement.cs
@@ -4,29 +4,96 @@ using UnityEngine;
 
 public class WeatherManagement : MonoBehaviour
 {
-    
+    public static WeatherManagement Instance; 
+
     public enum weatherType { 
         Sunny,
         Rainy,
         Foggy
     }
 
-    public weatherType weather;
-       
+    public weatherType currentWeather;
 
-    // Start is called before the first frame update
+    public ParticleSystem rain;
+    public Vector2 durationRange = new Vector2(60,180);
+    [SerializeField]private float timer;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+  
     void Start()
     {
-        
+        timer = RandomTimer();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            changeWeather(RandomState()); 
+        }
     }
 
-    public void changeWeather(weatherType thisWeather) { 
-        weather = thisWeather;
+
+
+    weatherType RandomState()
+    {  
+        int randomIndex = Random.Range(0, 3);
+        weatherType weatherType = (weatherType)Random.Range(0, 3);
+        return weatherType;
+    }
+
+    public void changeWeather(weatherType thisWeather) {
+
+        OnExitState();
+        currentWeather = thisWeather;
+        OnEnterState();
+    }
+
+    void OnExitState()
+    {
+        switch (currentWeather)
+        {
+            case weatherType.Sunny:
+                break;
+            case weatherType.Rainy:
+                rain.Stop();
+                break;
+            case weatherType.Foggy:
+                break;
+            default:
+                break;
+        }
+    }
+
+    float RandomTimer()
+    {
+        return Random.Range(durationRange.x, durationRange.y);
+    }
+
+    void OnEnterState()
+    {
+        timer = RandomTimer();
+
+        switch (currentWeather)
+        {
+            case weatherType.Sunny:
+                break;
+            case weatherType.Rainy:
+                rain.Play();
+                break;
+            case weatherType.Foggy:
+                break;
+            default:
+                break;
+        }
     }
 }
