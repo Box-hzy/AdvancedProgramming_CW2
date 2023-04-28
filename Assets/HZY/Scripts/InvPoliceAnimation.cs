929be8cd525c2f7792cd61a47f7be7621cc763a2
Box <heziyin@hotmail.com>
2023-04-29 01:39:36 +0800

    few things
    1. create fire station model
    2. inv police animation script
    3. adjust house collider
    4. add TMP

--- Assets/HZY/Scripts/InvPoliceAnimation.cs
+++ Assets/HZY/Scripts/InvPoliceAnimation.cs
@@ -5,27 +5,58 @@ using UnityEngine;
 public class InvPoliceAnimation : MonoBehaviour
 {
     PolicemanInvInCar policemanInvInCar;
-
+    Animator animator;
+    PolicemanInvInCar.State currentState;
 
     // Start is called before the first frame update
     void Start()
     {
         policemanInvInCar = GetComponentInParent<PolicemanInvInCar>();
+        animator = GetComponent<Animator>();
+        currentState = policemanInvInCar.getState();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StateAnimation();
     }
 
-    void OnStateEnter()
-    { 
-    
+    void StateAnimation()
+    {
+        currentState = policemanInvInCar.getState();
+
+        if (currentState != PolicemanInvInCar.State.Investigate)
+        {
+            animator.SetBool("Text", false);
+        }
+        else if (currentState != PolicemanInvInCar.State.Chase)
+        {
+            animator.SetBool("Run", false);
+        }
+        else if (currentState != PolicemanInvInCar.State.FindVillager && currentState != PolicemanInvInCar.State.BackToCar)
+        {
+            animator.SetBool("Walk", false);
+        }
+
+        switch (currentState)
+        {
+            case PolicemanInvInCar.State.FindVillager:
+                animator.SetBool("Walk", !policemanInvInCar.agent.isStopped);
+                break;
+            case PolicemanInvInCar.State.Investigate:
+                animator.SetBool("Text", true);
+                break;
+            case PolicemanInvInCar.State.Chase:
+                animator.SetBool("Run", !policemanInvInCar.agent.isStopped);
+                break;
+            case PolicemanInvInCar.State.BackToCar:
+                animator.SetBool("Walk", !policemanInvInCar.agent.isStopped);
+                break;
+            default:
+                break;
+        }
     }
 
-    void OnStateExit()
-    { 
-    
-    }
+   
 }
